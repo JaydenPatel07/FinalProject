fb55e8780cf9edefdac1ca57a8c28604f5b61ae2
patelj23 <patelj23@hsnet.ahsd.org>
2023-04-28 10:38:11 -0400

    Testing Commit

---
+++ app/src/main/java/com/example/finalproject/Game.kt
@@ -0,0 +1,4 @@
+package com.example.finalproject
+
+data class Game(val Opponent: String, val ahScore: Int, val opponentScore: Int, val date:String) {
+}
---
+++ app/src/main/java/com/example/finalproject/ViewHolderSchedule.kt
@@ -0,0 +1,13 @@
+package com.example.finalproject
+/*
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.databinding.ListItemLayoutBinding
+
+class CourseViewHolder(val binding: ListItemLayoutBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    private lateinit var game: Game
+
+}
+
+
+ */
