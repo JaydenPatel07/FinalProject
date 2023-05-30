7ceb2a922c30e7c4ef4ece59cb5bb47637c8c008
patel <jaydenpatel057@gmail.com>
2023-05-30 07:35:31 -0400

    Commit

--- app/src/main/java/com/example/finalproject/Game.kt
+++ app/src/main/java/com/example/finalproject/Game.kt
@@ -1,4 +1,4 @@
 package com.example.finalproject
 
-class Game (val homeName: String,val awayName: String,val homeScore:Int, val awayScore:Int, val homePicResourceID: Int, val awayPicResourceID: Int  )
+class Game (var date:String, var time:String, var homeName: String,var awayName: String,var homeScore:Int, var awayScore:Int, var homePicResourceID: Int, var awayPicResourceID: Int, var winOrNot:Boolean )
 
--- app/src/main/java/com/example/finalproject/GameHolder.kt
+++ app/src/main/java/com/example/finalproject/GameHolder.kt
@@ -1,14 +1,33 @@
 package com.example.finalproject
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
-import com.example.finalproject.databinding.ListItemLayoutIndividualBinding
 import com.example.finalproject.databinding.ListItemScheduleBinding
 
 class GameHolder(val binding: ListItemScheduleBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    private lateinit var currentPlayer: Player
+    private lateinit var currentGame: Game
 
     fun bindGame(game: Game) {
+        currentGame = game
+        binding.awayPic.setImageResource(currentGame.awayPicResourceID)
+        binding.homePic.setImageResource(currentGame.homePicResourceID)
+        binding.scoreHome.text = currentGame.homeScore.toString()
+        binding.awayScore.text = currentGame.awayScore.toString()
+        binding.awayText.text = currentGame.awayName
+        binding.homeText.text = currentGame.awayName
+
+        if(currentGame.winOrNot){
+            binding.scoreHome.setTextColor(Color.GREEN)
+            binding.awayScore.setTextColor(Color.GREEN)
+
+        }
+        else{
+            binding.awayScore.setTextColor(Color.RED)
+            binding.scoreHome.setTextColor(Color.RED)
+
+
+        }
 
 
     }
